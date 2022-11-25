f8089691de7dff8cfaceebcc129195798642ff26
mfudg395 <mfudg395@mtroyal.ca>
2022-11-25 18:27:34 +0000

    fixed geolocation route to work on server

--- geolocation-router.js
+++ geolocation-router.js
@@ -1,10 +1,3 @@
-/**
- * IMPORTANT: I've isolated the geolocation stuff to this file for now, just to keep it separate from our main server.js.
- * 
- * This file needs to be run from the command line instead of server.js for geolocation data to be served.
- * 
- * It can be easily moved elsewhere when we have a better idea on how to set up our routes.
- *  */ 
 const unirest = require("unirest");
 const express = require("express");
 const router = express.Router();
@@ -13,18 +6,20 @@ app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
 
 router.get("/geolocation", (req, res) => {
-    const apiCall = unirest("GET", "https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/");
+    const apiCall = unirest(
+        "GET",
+        "https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/"
+    );
     apiCall.query({
-      "ip": "142.109.127.37"
+	"ip": req["x-forwarded-for"] || req.socket.remoteAddress.replace(/^.*:/, ''),
     });
-
     apiCall.headers({
-      "x-rapidapi-host": "ip-geolocation-ipwhois-io.p.rapidapi.com",
-      "x-rapidapi-key": "6960f52de6msh9db305e6cdd65fbp1965aejsn85a8048f9529",
-      "useQueryString": true
+        "x-rapidapi-host": "ip-geolocation-ipwhois-io.p.rapidapi.com",
+        "x-rapidapi-key": "6960f52de6msh9db305e6cdd65fbp1965aejsn85a8048f9529",
+	"useQueryString": true
     });
-
-    apiCall.end(function (result) {
-      res.send(result.body);
-    })
+    apiCall.end(function(result) {
+        if (res.error) throw new Error(result.error);
+        res.send(result.body);
+    });
 });
