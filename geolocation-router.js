643784cfe811e04a934711eb4eb65394c8d0cbca
Matthew Fudge <mfudg395@mtroyal.ca>
2022-11-25 10:41:07 -0700

    Implemented geolocation API route

--- geolocation-router.js
+++ geolocation-router.js
@@ -5,6 +5,7 @@
  * 
  * It can be easily moved elsewhere when we have a better idea on how to set up our routes.
  *  */ 
+const unirest = require("unirest");
 const express = require("express");
 const router = express.Router();
 const app = express();
@@ -12,5 +13,18 @@ app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
 
 router.get("/geolocation", (req, res) => {
-  res.send("Hello World");
+    const apiCall = unirest("GET", "https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/");
+    apiCall.query({
+      "ip": "142.109.127.37"
+    });
+
+    apiCall.headers({
+      "x-rapidapi-host": "ip-geolocation-ipwhois-io.p.rapidapi.com",
+      "x-rapidapi-key": "6960f52de6msh9db305e6cdd65fbp1965aejsn85a8048f9529",
+      "useQueryString": true
+    });
+
+    apiCall.end(function (result) {
+      res.send(result.body);
+    })
 });
