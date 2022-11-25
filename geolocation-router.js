af605492bf559ad5ba62ea0b16a3818e4f20a8c9
Matthew Fudge <mfudg395@mtroyal.ca>
2022-11-25 11:30:36 -0700

    fixed indentation

--- geolocation-router.js
+++ geolocation-router.js
@@ -7,19 +7,19 @@ app.listen(5000, () => console.log("Server Running"));
 
 router.get("/geolocation", (req, res) => {
     const apiCall = unirest(
-        "GET",
-        "https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/"
+      "GET",
+      "https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/"
     );
     apiCall.query({
-	"ip": req["x-forwarded-for"] || req.socket.remoteAddress.replace(/^.*:/, ''),
+	    "ip": req["x-forwarded-for"] || req.socket.remoteAddress.replace(/^.*:/, ''),
     });
     apiCall.headers({
-        "x-rapidapi-host": "ip-geolocation-ipwhois-io.p.rapidapi.com",
-        "x-rapidapi-key": "6960f52de6msh9db305e6cdd65fbp1965aejsn85a8048f9529",
-	"useQueryString": true
+      "x-rapidapi-host": "ip-geolocation-ipwhois-io.p.rapidapi.com",
+      "x-rapidapi-key": "6960f52de6msh9db305e6cdd65fbp1965aejsn85a8048f9529",
+	    "useQueryString": true
     });
     apiCall.end(function(result) {
-        if (res.error) throw new Error(result.error);
-        res.send(result.body);
+      if (res.error) throw new Error(result.error);
+      res.send(result.body);
     });
 });
