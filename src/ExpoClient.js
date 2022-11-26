a04d158eb6141e8b21f1af25081dc00e0ea90beb
Dylan Borchert <dborc610@mtroyal.ca>
2022-11-26 10:36:58 -0700

    added route to find user's friends

--- src/ExpoClient.js
+++ src/ExpoClient.js
@@ -40,19 +40,19 @@ module.exports.expoPush = (app) => {
         // Send the chunks to the Expo push notification service. There are
         // different strategies you could use. A simple one is to send one chunk at a
         // time, which nicely spreads the load out over time:
-        for (let chunk of chunks) {
-            try {
-            let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-                console.log(ticketChunk);
-                tickets.push(...ticketChunk);
-            // NOTE: If a ticket contains an error code in ticket.details.error, you
-            // must handle it appropriately. The error codes are listed in the Expo
-            // documentation:
-            // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
-            } catch (error) {
-                console.error(error);
+            for (let chunk of chunks) {
+                try {
+                let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+                    console.log(ticketChunk);
+                    tickets.push(...ticketChunk);
+                // NOTE: If a ticket contains an error code in ticket.details.error, you
+                // must handle it appropriately. The error codes are listed in the Expo
+                // documentation:
+                // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+                } catch (error) {
+                    console.error(error);
+                }
             }
-        }
         })();
 
 
